c79eb1a3c22f31edb10ffc988f4cbfd25fe9b16a
DmitryTim <51434449+DmitryTim@users.noreply.github.com>
2019-11-13 11:01:12 +0300

    Базовый синтаксис

---
+++ variables/main.js
@@ -0,0 +1,24 @@
+/*------------1----Homework--------------------*/
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+
+let average = ((algebra+geography+physics)/3);
+
+console.log (Math.trunc(average));
+/*-------------2---Homework-------------------*/
+
+let myName = "Dmitry";
+
+let message = (`Привет, мир! Меня зовут ${myName} Средняя оценка по предметам ${Math.trunc(average)}`);
+
+console.log (message);
+
+/*-------------3---Homework-------------------*/
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = ((x * y) + (5 * z) + (x - 1));
+
+console.log(result);
